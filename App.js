630726c7ff8eb1c462b045930f71dabb958a610c
andreevanatasha <tinskimmings@gmail.com>
2017-10-21 00:29:08 +0300

    Add Roboto font to the project

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View, TextInput, TouchableHighlight, Image, StatusBar
 import { Button } from './Button';
 import SlideTextInput from './SlideTextInput';
 import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+import {responsiveFontSize} from 'react-native-responsive-dimensions';
+import { Font } from 'expo';
+const Dimensions = require('Dimensions');
 
 export default class App extends React.Component {
     constructor(props) {
@@ -23,18 +26,24 @@ export default class App extends React.Component {
             '#ddf',
             '#ffbbd3',
             '#cbfcf4',
-            '#ad9595'
+            '#f7f5f5'
             ],
             backgrounds_number: 3,
             orientation: 'portrait',
+            fontLoaded: false
         };
 
         this.clearInput = this.clearInput.bind(this);
         this.onSwipeLeft = this.onSwipeLeft.bind(this);
         this.onSwipeRight = this.onSwipeRight.bind(this);
+        this.closeFullScreen = this.closeFullScreen.bind(this);
+        this.openFullScreen = this.openFullScreen.bind(this);
+
     }
 
     clearInput() {
+        this.refs.input.blur();
+        this.refs.input.clear();
         this.setState({text: ''});
     }
 
@@ -60,35 +69,87 @@ export default class App extends React.Component {
         };
     }
 
+    openFullScreen() {
+        this.setState({fullscreen: true});
+    }
+
+    closeFullScreen() {
+        this.setState({fullscreen: false});
+    }
+
+
+    async componentDidMount() {
+        await Font.loadAsync({
+          'Roboto': require('./img/Roboto-Bold.ttf'),
+      });
+
+        this.setState({ fontLoaded: true });
+    }
+
     render() {
-        let background_color, placeholder_color;
+        let background_color, screenWidth, screenHeight, placeholder_color;
         background_color = this.state.backgrounds[this.state.background_id];
         placeholder_color = this.state.placeholder_colors[this.state.background_id];
+        screenWidth = Dimensions.get('window').width;
+        screenHeight = Dimensions.get('window').height;
 
-        return (
-            <GestureRecognizer 
-                onSwipeLeft= { (state) => this.onSwipeLeft(state) }
-                onSwipeRight= { (state) => this.onSwipeRight(state) } 
-                style={{flex: 1}}>
-            <View style={[styles.main, {backgroundColor: background_color}]} >
-               <StatusBar barStyle="light-content" /> 
-                <View style={[styles.header, {backgroundColor: background_color}]}>
-                    <Button name='clear' disable={this.state.text} action={this.clearInput} />
-                    <Text> {this.state.text} </Text>
-                    <Button name='run' disable={this.state.text} action={this.openFullScreen}/>
+        if (this.state.fullscreen === false) { 
+            return (
+                <GestureRecognizer 
+                    onSwipeLeft= { (state) => this.onSwipeLeft(state) }
+                    onSwipeRight= { (state) => this.onSwipeRight(state) } 
+                    style={{flex: 1}}>
+                <View style={[styles.main, {backgroundColor: background_color}]} >
+                   <StatusBar barStyle="light-content" /> 
+                    <View style={[styles.header, {backgroundColor: background_color}]}>
+                        <Button name='clear' disable={this.state.text} action={this.clearInput} />
+                    {/*<Text>{this.state.text}</Text>*/}
+                        <Button name='run' disable={this.state.text} action={this.openFullScreen}/>
+                    </View>
+
+                    {
+                        this.state.fontLoaded ? (
+                            <SlideTextInput 
+                            style={styles.input} 
+                            ref='input'
+                            onChangeText={(text) => this.setState({text: text})}
+                            placeholder={this.state.placeholder} 
+                            value={this.state.text} 
+                            multiline={true} 
+                            placeholderTextColor={placeholder_color}
+                            returnKeyType='done' 
+                            blurOnSubmit={true} />
+                            ) : null
+                    }
                 </View>
-                <SlideTextInput 
-                    style={[styles.input, {backgroundColor: background_color}]} 
-                    onChangeText={(text) => this.setState({text: text})}
-                    placeholder={this.state.placeholder} 
-                    value={this.state.text} 
-                    multiline={true} 
-                    placeholderTextColor={placeholder_color}
-                    returnKeyType='done' 
-                    blurOnSubmit={true} />
-            </View>
-            </GestureRecognizer>
-          );
+                </GestureRecognizer>
+              );
+        } else {
+            return (
+                <GestureRecognizer 
+                    onSwipeLeft= { (state) => this.onSwipeLeft(state) }
+                    onSwipeRight= { (state) => this.onSwipeRight(state) } 
+                    style={{flex: 1}}>
+                <View style={[styles.main, {backgroundColor: background_color}]} >
+                   <StatusBar barStyle="light-content" /> 
+                    <View style={styles.header}>
+                        <Button name='close' disable={this.state.text} action={this.closeFullScreen}/>
+                    </View>
+                    <View style={{width: '94%', flex: 1}}>
+                    {
+                        this.state.fontLoaded ? (
+                            <Text
+                            adjustsFontSizeToFit={true}
+                            style={[styles.input, styles.responsive]} >
+                            {this.state.text}
+                            </Text>
+                            ) : null
+                    }
+                    </View>
+                </View>
+                </GestureRecognizer>
+            );
+        }
     }
 }
 
@@ -111,17 +172,28 @@ header: {
   },
 
 input: {
+    width: '94%',
+    flex: 1,
+
   fontSize: 50,
   lineHeight: 1,
-  width: '94%',
-  flex: 1,
-
-  fontFamily: 'Helvetica Neue',
+  fontFamily: 'Roboto',
   fontStyle: 'normal',
   fontWeight: '900',
   color: '#ffffff',
 
   overflow: 'hidden',
+
+    },
+
+responsive: {
+    fontSize: responsiveFontSize(5),
+    //fontSize: 100,
+  lineHeight: 1,
+  fontFamily: 'Roboto',
+  fontStyle: 'normal',
+  fontWeight: '900',
+  color: '#ffffff',
 
 },
 });
